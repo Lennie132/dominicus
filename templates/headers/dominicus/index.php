f3452ebada50750d42a01b9449271f6466759350
Lennie132 <lennart.v@live.nl>
2016-09-21 14:05:34 +0200

    Added dropdown menu, improved menu behavior

--- templates/headers/dominicus/index.php
+++ templates/headers/dominicus/index.php
@@ -14,30 +14,26 @@ lcms_client_script::add_header_js('main.js');
             </div>
 
             <?php // lcms::Menu()->setNiveausDiep(2)->setClass('main-nav list-unstyled')->getHTML(); ?>
-            <?= get_menu(0, '', 'header-nav nav-ul list-unstyled'); ?>
+            <?= get_menu(0, '', 'header-nav list-unstyled'); ?>
 
-            <div class="header-social" >
-                <div class="social" id="social">
-                    <ul class="social-list">
-                        <?php
-                        $socialmedia = get_artikelen_arr('art_socialmedia', '*', 'a.gewicht DESC');
-                        //print_pre($socialmedia);
+            <ul class="social-list">
+                <?php
+                $socialmedia = get_artikelen_arr('art_socialmedia', '*', 'a.gewicht DESC');
+                //print_pre($socialmedia);
 
-                        if (!empty($socialmedia)) {
-                            foreach ($socialmedia as $social) {
-                                ?>
-                                <li class="social-item">
-                                    <a href="<?php echo lcms::niceLink($social['link']); ?>" target="_blank" class="social-link" title="<?php echo $social['naam']; ?>">
-                                        <span class="<?php echo $social['icon']; ?>"></span>
-                                    </a>
-                                </li>
-                                <?php
-                           }
-                       }
+                if (!empty($socialmedia)) {
+                    foreach ($socialmedia as $social) {
                         ?>
-                    </ul>
-                </div>
-            </div>
+                        <li class="social-item">
+                            <a href="<?php echo lcms::niceLink($social['link']); ?>" target="_blank" class="social-link" title="<?php echo $social['naam']; ?>">
+                                <span class="<?php echo $social['icon']; ?>"></span>
+                            </a>
+                        </li>
+                        <?php
+                    }
+                }
+                ?>
+            </ul>
         </div>
 
     </div>
--- templates/headers/dominicus/main.js
+++ templates/headers/dominicus/main.js
@@ -14,45 +14,51 @@ $(document).ready(function () {
         } // executed when item is moved back to main menu);
     });
     // Menu button
-    $('.menu-trigger').on('click', function () {
-        console.log("test menu");
-        $('html').toggleClass('menu__is-open');
-        if ($('html').hasClass('menu__is-open')) {
-            console.log("open");
+//    $('.menu-trigger').on('click', function () {
+//        console.log("test menu");
+//        $('html').toggleClass('menu__is-open');
+//        if ($('html').hasClass('menu__is-open')) {
+//            console.log("open");
+//
+//        } else {
+//            console.log("dicht");
+//            $('nav > ul > li.isparent').removeClass('is-open-1');
+//            $('nav > ul > li.isparent').removeClass('is-open-2');
+//            $('nav > ul > li.isparent > a').removeClass('is-navigatable');
+//            $('nav > ul> li.isparent > ul > li.isparent > a').removeClass('is-navigatable');
+//        }
+//    });
 
+    $('.header-nav > li.isparent > a').on('click', function (e) {
+        e.preventDefault();
+        if ($(this).parents('li.isparent').eq(0).hasClass('is-open-1')) {
+            console.log ('sluit');
+            $(this).parents('li.isparent').eq(0).removeClass('is-open-1');
         } else {
-            console.log("dicht");
-            $('nav > ul > li.isparent').removeClass('is-open-1');
-            $('nav > ul > li.isparent').removeClass('is-open-2');
-            $('nav > ul > li.isparent > a').removeClass('is-navigatable');
-            $('nav > ul> li.isparent > ul > li.isparent > a').removeClass('is-navigatable');
+            console.log ('open');
+            $('.nav__dropdown > li.isparent').removeClass('is-open-1');
+            $(this).parents('li.isparent').eq(0).addClass('is-open-1');
         }
     });
 
+    $('.nav__dropdown > li.isparent > a').on('click', function (e) {
+        e.preventDefault();
+        console.log('click');
 
 
-    $('.nav-ul > li.isparent > a').on('click', function (e) {
-            if ($(this).parents('.nav-ul > li.isparent').eq(0).hasClass('is-open-1')) {
-                $(this).parents('.nav-ul > li.isparent').eq(0).removeClass('is-open-1');
-            } else {
-                $('.nav-ul > li.isparent').removeClass('is-open-1');
-                $(this).parents('.nav-ul > li.isparent').eq(0).addClass('is-open-1');
-            }
+//        $('.priority-nav__dropdown > li.isparent > ul > li.isparent > a').on('click', function (e) {
+//
+//        });
 
-            $('.nav-ul> li.isparent > ul > li.isparent > a').on('click', function (e) {
+        //Link uitschakelen
+        $(this).addClass('is-navigatable');
+        if ($(this).hasClass('is-navigatable')) {
+            e.preventDefault();
+        }
 
-
-            });
-
-            //Link uitschakelen
-            $(this).addClass('is-navigatable');
-            if ($(this).hasClass('is-navigatable')) {
-                e.preventDefault();
-            }
-
-            $('nav > ul > li.isparent > a').addClass('is-navigatable');
+        $('nav > ul > li.isparent > a').addClass('is-navigatable');
 
     });
-    
-    
+
+
 });
