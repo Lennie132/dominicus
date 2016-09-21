690249cc34d847dd19cd019a4781ae8949c416fa
Lennie132 <lennart.v@live.nl>
2016-09-21 15:29:54 +0200

    fixed sfeerbeeld size

--- templates/paginas/standaard/template.php
+++ templates/paginas/standaard/template.php
@@ -1,26 +1,27 @@
 <?php
-/** Hier eventeel nog een sfeerbeeld of kruimels o.i.d. inladen **/
+/** Hier eventeel nog een sfeerbeeld of kruimels o.i.d. inladen * */
 $sfeer = get_sfeerafbeelding($DATA['page'], 1);
 
-/** Hier wordt het grid met de content ingelezen **/
+/** Hier wordt het grid met de content ingelezen * */
 //echo $this->buildHTMLgrid($DATA['page']);
 
-/** Individuele content inlezen **/
+/** Individuele content inlezen * */
 //echo lcms::Template()->getSectieContent($DATA['page'], '<id van sectie>');
-
 ?>
 
 <?php
 if (trim($sfeer) != '') {
     ?>
-    <div class="sfeerbeeld-wrapper-335 sfeerbeeld" style="background-image: url('<?= lcms::resize($sfeer, 1920, 335, '', 80); ?>');"></div>
+    <div class="sfeerbeeld" style="background-image: url('<?= lcms::resize($sfeer, 1920, 320, '1920x320', 80); ?>');"></div>
     <?php
 }
 ?>
 
 <div class="titel-wrapper">
-    <div class="col-xs-12">
-        <h1><?php echo get_pagina_title($pageid) ?></h1>
+    <div class="row">
+        <div class="col-xs-12">
+            <h1><?php echo get_pagina_title($pageid) ?></h1>
+        </div>
     </div>
 </div>
 
