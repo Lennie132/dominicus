bcc90ed4fab86aa898a1bada2d9c794b6d54480b
Lennie132 <lennart.v@live.nl>
2016-09-22 17:14:39 +0200

    made header fixed at large screen

--- templates/headers/dominicus/index.php
+++ templates/headers/dominicus/index.php
@@ -7,37 +7,37 @@ lcms_client_script::add_header_js('main.js');
 ?>
 <header>
     <div class="header-wrapper">
-        <div class="header-main">
-            <div class="header-logo">
-                <?php echo get_logo_html(); ?>
-            </div>
+        <div class="header-bar">
+            <div class="header-main">
+                <div class="header-logo">
+                    <?php echo get_logo_html(); ?>
+                </div>
 
-            <?php // lcms::Menu()->setNiveausDiep(2)->setClass('main-nav list-unstyled')->getHTML(); ?>
-            <?= get_menu(0, '', 'header-nav list-unstyled'); ?>
+                <?php // lcms::Menu()->setNiveausDiep(2)->setClass('main-nav list-unstyled')->getHTML(); ?>
+                <?= get_menu(0, '', 'header-nav list-unstyled'); ?>
 
-            <ul class="social-list">
-                <?php
-                $socialmedia = get_artikelen_arr('art_socialmedia', '*', 'a.gewicht DESC');
-                //print_pre($socialmedia);
+                <ul class="social-list">
+                    <?php
+                    $socialmedia = get_artikelen_arr('art_socialmedia', '*', 'a.gewicht DESC');
+                    //print_pre($socialmedia);
 
-                if (!empty($socialmedia)) {
-                    foreach ($socialmedia as $social) {
-                        ?>
-                        <li class="social-item">
-                            <a href="<?php echo lcms::niceLink($social['link']); ?>" target="_blank" class="social-link" title="<?php echo $social['naam']; ?>">
-                                <span class="<?php echo $social['icon']; ?>"></span>
-                            </a>
-                        </li>
-                        <?php
+                    if (!empty($socialmedia)) {
+                        foreach ($socialmedia as $social) {
+                            ?>
+                            <li class="social-item">
+                                <a href="<?php echo lcms::niceLink($social['link']); ?>" target="_blank" class="social-link" title="<?php echo $social['naam']; ?>">
+                                    <span class="<?php echo $social['icon']; ?>"></span>
+                                </a>
+                            </li>
+                            <?php
+                        }
                     }
-                }
-                ?>
-                <li class="social-item">
-                    <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FDominicusCollege&width=116&layout=button_count&action=like&show_faces=false&share=false&height=21&appId" width="130" height="21" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-                </li>
-
-            </ul>
+                    ?>
+                    <li class="social-item">
+                        <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FDominicusCollege&width=116&layout=button_count&action=like&show_faces=false&share=false&height=21&appId" width="130" height="21" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                    </li>
+                </ul>
+            </div>
         </div>
-
     </div>
 </header>
