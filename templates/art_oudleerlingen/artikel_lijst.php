393948213eec57313462181d5f77008e38e3e060
Lennie132 <lennart.v@live.nl>
2016-09-22 14:17:16 +0200

    improved old-student block style

--- templates/art_oudleerlingen/artikel_lijst.php
+++ templates/art_oudleerlingen/artikel_lijst.php
@@ -23,59 +23,55 @@ $art_arr = get_artikelen_arr($config['tabelnaam'], $config['group_id'], $config[
 //print_pre($DATA);
 if (!empty($art_arr)) {
     ?>
-    <div class="student-wrapper">
-        <div class="row">
-            <?php
-            foreach ($art_arr as $key => $artikel) {
-                $img = get_art_file_path($artikel['afbeelding'], $artikel['artikel_id']);
-                ?>
-                <div class="col-lg-4 col-md-6 col-sm-6">
-                    <div class="student-item">
-                        <?php
-                        if (trim($img != '')) {
-                            ?>
-                            <div class="student-afbeelding col-sm-2 hidden-xs">
-                                <img src="<?= lcms::resize($img, 140, 200, '140x200', 80); ?>" class="" alt="<?= $artikel['Titel']; ?>" />
-                            </div>
-                            <?php
-                        }
-                        ?>
-                        <div class="student-content col-sm-10 col-xs-12">
-                            <h2 class="student-titel"><?= $artikel['titel']; ?></h2>
-                            <div class="student-img-space col-sm-3 hidden-xs">
-
-                            </div>
-                            <div class="student-info col-sm-9">
-                                <p class="student-naam"><?= $artikel['naam']; ?></p>
-                                <p class="student-jaar-leerling">Dominicus leerling in: <?= $artikel['jaar_dominicusleerling']; ?></p>
-                                <p class="student-jaar-interview-beroep">Anno <?= $artikel['jaar_interview']; ?>: <?= $artikel['beroep']; ?></p>
-                                <div class="student-tekst-intro">
-                                    Anno <?= $artikel['tekst_intro']; ?>
-                                </div>
-                                <a class="read_more" href="<?= link::c($DATA['page'])->artikel_groep($artikel['page'])->artikel_id($artikel['artikel_id']); ?>" title="<?= $artikel['Titel']; ?>">
-                                    Lees meer<span class="icon-chevron_right"></span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <?php
+    foreach ($art_arr as $key => $artikel) {
+        $img = get_art_file_path($artikel['afbeelding'], $artikel['artikel_id']);
+        ?>
+        <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="student-item">
                 <?php
-            }
-            ?>
-        </div>
-        <?php if (count($art_arr) > 10) { ?>
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="pagination">
-                        <?php
-                        limit_links('', true);
-                        clear_limit();
-                        ?>
+                if (trim($img != '')) {
+                    ?>
+                    <div class="student-afbeelding col-sm-2 hidden-xs">
+                        <img src="<?= lcms::resize($img, 140, 200, '140x200', 80); ?>" class="" alt="<?= $artikel['Titel']; ?>" />
+                    </div>
+                    <?php
+                }
+                ?>
+                <div class="student-content col-sm-10 col-xs-12">
+                    <h2 class="student-titel"><?= $artikel['titel']; ?></h2>
+                    <div class="student-img-space col-sm-3 hidden-xs">
+
+                    </div>
+                    <div class="student-info col-sm-9">
+                        <p class="student-naam"><?= $artikel['naam']; ?></p>
+                        <p class="student-jaar-leerling">Dominicus leerling in: <?= $artikel['jaar_dominicusleerling']; ?></p>
+                        <p class="student-jaar-interview-beroep">Anno <?= $artikel['jaar_interview']; ?>: <?= $artikel['beroep']; ?></p>
+                        <div class="student-tekst-intro">
+                            Anno <?= $artikel['tekst_intro']; ?>
+                        </div>
+                        <a class="read_more" href="<?= link::c($DATA['page'])->artikel_groep($artikel['page'])->artikel_id($artikel['artikel_id']); ?>" title="<?= $artikel['Titel']; ?>">
+                            Lees meer<span class="icon-chevron_right"></span>
+                        </a>
                     </div>
                 </div>
             </div>
-        <?php } ?>
-    </div>
+        </div>
+        <?php
+    }
+    ?>
+    <?php if (count($art_arr) > 10) { ?>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="pagination">
+                    <?php
+                    limit_links('', true);
+                    clear_limit();
+                    ?>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
     <?php
 }
 ?>
--- templates/art_oudleerlingen/main.js
+++ templates/art_oudleerlingen/main.js
@@ -2,6 +2,4 @@ $(document).ready(function () {
     $(".student-tekst-intro").dotdotdot({
         watch: "window"
     });
-
-
 });
