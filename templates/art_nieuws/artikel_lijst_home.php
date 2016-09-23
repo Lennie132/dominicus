e67d1b19623a74ba81d23c781528f4b3af9f20f7
Lennie132 <lennart.v@live.nl>
2016-09-23 13:51:10 +0200

    added news to home-page with other design

---
+++ templates/art_nieuws/artikel_lijst_home.php
@@ -0,0 +1,55 @@
+<?php
+smart_include_css('css/style.less');
+smart_include_css('css/style.css');
+smart_include_js('main.js');
+
+$config = array(
+    'tabelnaam' => $tabelnaam,
+    'group_id' => $DATA['group'],
+    'order' => 't.datum DESC', // bv: RAND()
+    'artikel_id' => 0,
+    'limit_links' => 3,
+    'where' => ''
+);
+
+/* Artikelen ophalen */
+
+
+
+
+
+$art_arr = get_artikelen_arr($config['tabelnaam'], $config['group_id'], $config['order'], $config['artikel_id'], $config['limit_links'], $config['where']);
+//print_pre($art_arr);
+//print_pre($DATA);
+if (!empty($art_arr)) {
+    ?>
+    <div class="row">
+        <?php
+        foreach ($art_arr as $key => $artikel) {
+            $oldDate = $artikel['Datum'];
+            $newDate = date("d-m-Y", strtotime($oldDate));
+            ?>
+            <div class="col-xs-12">
+                <div class="newsitem-home">
+                    <div class="col-xs-12">
+                        <h3 class="newsitem-title"><?= $newDate; ?> | <?= $artikel['Titel']; ?></h3>
+                    </div>
+                    <div class="col-xs-12 newsitem-intro">
+                        <p class="newsitem-intro-p"><?= $artikel['Inleiding']; ?></p>
+                    </div>
+                    <div class="col-xs-12">
+                        <a class="read_more" href="<?= link::c($DATA['page'])->artikel_groep($artikel['page'])->artikel_id($artikel['artikel_id']); ?>" title="<?= $artikel['Titel']; ?>">
+                        Lees meer<span class="icon-chevron_right"></span>
+                    </a>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+    <?php
+}
+?>
--- templates/art_nieuws/main.js
+++ templates/art_nieuws/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-
+    $(".newsitem-intro").dotdotdot({
+        watch: "window"
+    });
     
     
 });
