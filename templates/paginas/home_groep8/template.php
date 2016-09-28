013752c046b8bd8bcbad9e6b7e01921965b0777b
Lennie132 <lennart.v@live.nl>
2016-09-28 13:35:17 +0200

    improvements and progress on groep8-home page

--- templates/paginas/home_groep8/template.php
+++ templates/paginas/home_groep8/template.php
@@ -18,17 +18,18 @@
 
         <div class="col-md-4 col-sm-6 block-wrapper">
             <div class="block-hallo-achtste-groeper">
-                <img class="icon-op-zoek" src="img/icon-op-zoek.png">
+                <img class="icon-duim" src="<?= lcms::resize('img/icon-duim.png', 60, 60, '', 80); ?>">
                 <?php echo lcms::Template()->getSectieContent($DATA['page'], 'hallo-achtste-groeper'); ?>
             </div>
         </div>
 
         <div class="col-md-4 col-sm-6 block-wrapper">
             <div class="block-schrijf-in">
-                <img class="icon-schrijf-in" src="img/icon-kom-kijken.png">
+                <img class="icon-inschrijven" src="<?= lcms::resize('img/icon-inschrijven.png', 45, 45, '', 80); ?>">
                 <?php echo lcms::Template()->getSectieContent($DATA['page'], 'schrijf-in'); ?>
             </div>
             <div class="block-kom-kijken-groep8">
+                <img class="icon-kom-kijken" src="<?= lcms::resize('img/icon-kom-kijken.png', 65, 65, '', 80); ?>">
                 <?php echo lcms::Template()->getSectieContent($DATA['page'], 'kom-kijken'); ?>
             </div>
         </div>
@@ -40,12 +41,12 @@
         <div class="col-md-4 block-wrapper">
             <div class="block block-filmmakers">
                 <?php echo lcms::Template()->getSectieContent($DATA['page'], 'filmmakers'); ?>
-                <img class="icon-filmmakers" src="img/icon-filmmakers.png">
+                <img class="icon-filmmakers" src="<?= lcms::resize('img/icon-filmmakers.png', 50, 50, '', 80); ?>">
             </div>
         </div>
         <div class="col-md-4 block-wrapper">
             <div class="block block-10-vragen">
-                <img class="icon-10-vragen" src="img/icon-10-vragen.png">
+                <img class="icon-vragen" src="<?= lcms::resize('img/icon-vragen.png', 100, 100, '', 80); ?>">
                 <?php echo lcms::Template()->getSectieContent($DATA['page'], '10-vragen'); ?>
             </div>
         </div>
