d6e31bb02ec9ba4dfbf14cfff66b9461b51b028a
Lennie132 <lennart.v@live.nl>
2016-10-07 14:21:00 +0200

    added default sfeerbeeld for new pages

--- templates/paginas/standaard/template.php
+++ templates/paginas/standaard/template.php
@@ -10,26 +10,30 @@ $sfeer = get_sfeerafbeelding($DATA['page'], 1);
 ?>
 
 <?php
-if (trim($sfeer) != '') {
-    ?>
-    <div class="sfeerbeeld" style="background-image: url('<?= lcms::resize($sfeer, 1920, 320, '1920x320', 80); ?>');"></div>
-    <?php
+if (trim($sfeer) == '') {
+  $sfeer = 'img/standaard-sfeerbeeld.jpg';
 }
 ?>
+<div class="sfeerbeeld" style="background-image: url('<?= lcms::resize($sfeer, 1920, 320, '1920x320', 80); ?>');"></div>
 
 <div class="titel-wrapper">
-    <div class="containter">
-        <div class="container">
-            <div class="col-xs-12">
-                <h1><?php echo get_pagina_title($pageid) ?></h1>
-            </div>
-        </div>
+  <div class="containter">
+    <div class="container">
+      <div class="col-xs-12">
+        <h1><?php echo get_pagina_title($pageid) ?></h1>
+      </div>
     </div>
+  </div>
 </div>
 
 <div class="content-wrapper">
-    <?php
-    /** Hier wordt het grid met de content ingelezen * */
-    echo $this->buildHTMLgrid($DATA['page']);
-    ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12"><?php
+      /** Hier wordt het grid met de content ingelezen * */
+      echo get_all_content_html();
+      ?></div>
+    </div>
+  </div>
+
 </div>
