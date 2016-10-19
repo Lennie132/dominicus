c9e77ba0a75ff930335f50442961fec42c0c1376
Lennie132 <lennart.v@live.nl>
2016-10-19 16:33:36 +0200

    made photoalbum compatible with multi-file upload and improved style

--- templates/art_fotoalbum/homepage_slider.php
+++ templates/art_fotoalbum/homepage_slider.php
@@ -1,30 +1,34 @@
 <?php
-$config = array(
-    'tabelnaam' => 'art_fotoalbum',
-    'group_id' => '*',
-    'order' => ' a.artikel_id DESC', // bv: RAND()
-    'artikel_id' => 0, 
-   'limit_links' => 10,
-    'where' => ' GROUP BY g.group_id ',
-    'module' => 0,
-);
+  $config = array(
+      'tabelnaam' => 'art_fotoalbum',
+      'group_id' => '*',
+      'order' => ' a.artikel_id DESC', // bv: RAND()
+      'artikel_id' => 0,
+      'limit_links' => 1,
+      'where' => ' GROUP BY g.group_id ',
+      'module' => 0,
+  );
 
-$art_arr = get_artikelen_arr($config['tabelnaam'], $config['group_id'], $config['order'], $config['artikel_id'], $config['limit_links'], $config['where']);
-if (!empty($art_arr)) {
+  $art_arr = get_artikelen_arr($config['tabelnaam'], $config['group_id'], $config['order'], $config['artikel_id'], $config['limit_links'], $config['where']);
+  if (!empty($art_arr)) {
     ?>
     <div class="slider-wrapper">
-        <div class="bxslider">
-            <?php
-            foreach ($art_arr as $key => $artikel) {
-                $img = get_art_file_path($artikel['afbeelding'], $artikel['artikel_id']);
-                if (trim($artikel['afbeelding']) != '') {
-                    ?>
-                    <div class="slide" style="background-image:url('<?= lcms::resize($img, 1920, 1080, '', 80); ?>');"> 
-                    </div>
-                    <?php
-                }
+      <div class="bxslider">
+        <?php
+        foreach ($art_arr as $key => $artikel) {
+          $images = get_art_multi_files($artikel['afbeeldingen'], $artikel['artikel_id'], $DATA['m']);
+          //print_pre($images);
+          foreach ($images as $key => $image) {
+            //print_pre($image);
+            if (trim($image['path']) != '') {
+              ?>
+              <div class="slide" style="background-image:url('<?= lcms::resize($image['path'], 1920, 1080, '', 80); ?>');"> 
+              </div>
+              <?php
             }
-            ?>
-        </div>
+          }
+        }
+        ?>
+      </div>
     </div>
-<?php } ?>
+  <?php } ?>
