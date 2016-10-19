4ceced725a462a4fd7d5d836ddb37072b7b2b92c
Lennie132 <lennart.v@live.nl>
2016-10-19 16:35:26 +0200

    made photoalbum compatible with multi-file upload and improved style

---
+++ templates/art_fotoalbum/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+  //-- Slider
+  $('.bxslider').bxSlider({
+    // Automatisch sliden als er meer dan 1 slide is
+    auto: ($('.bxslider > .slide').not('.bxslider > .bx-clone').length > 1) ? true : false,
+    pager: false,
+    controls: false,
+    speed: 2000,
+    pause: 5000
+  });
+
+  //-- Fotoalbum
+  $('.fancybox').fancybox({
+    openEffect: 'elastic',
+    closeEffect: 'elastic'
+  });
+
+});
